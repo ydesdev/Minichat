de39c744f19d36f32e5138e136a70d3e76731948
Yann Desdevises <ydesdev@hotmail.com>
2018-01-18 11:57:21 +0100

    Refactoring minichat single page

--- minichat.php
+++ minichat.php
@@ -1,3 +1,26 @@
+<?php
+$bdd = new PDO('mysql:host=localhost;dbname=minichat', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$error = "";
+if (!empty($_POST['PseudoName']) && !empty($_POST['MessageContent'])) {
+
+	$NewMessage = $bdd->prepare('INSERT INTO minichat(Pseudo, Message) VALUES(:Pseudo, :Message)');
+	$NewMessage->execute(array(
+	'Pseudo'=> ($_POST['PseudoName']),
+	'Message'=> ($_POST['MessageContent'])
+	));
+}	else {
+
+	if(isset($_POST['PseudoName'])){
+     $error =  "Veuillez entrer un pseudo et un message";
+	}
+}	
+
+$ChatThread = $bdd->query('SELECT UPPER(Pseudo) AS Pseudo_maj, Message FROM minichat ORDER BY ID DESC LIMIT 10');
+
+
+
+?>
+
 <!DOCTYPE html>
 
 <html>
@@ -22,12 +45,13 @@
   <section class="Participer">
   	<fieldset>
   		<legend> Participer au miaou </legend>
-  			<form method="post" action="minichat_post.php">
-  			<label for="PseudoName">Pseudo: </label><br/>
-  			<input type= "text" name="PseudoName" id="PseudoNameField" /><br/>
-  			<label for="MessageContent"> dit: </label>
-  			<input type="text" name="MessageContent" id="MessageContent" />
-  			<input type="submit" value="Miaou"/> 
+  			<form method="post" action="minichat.php">
+  				<p style="color:red;"> <?= $error ?></p>
+  				<label for="PseudoName">Pseudo: </label><br/>
+  				<input type= "text" name="PseudoName" id="PseudoNameField" /><br/>
+  				<label for="MessageContent"> dit: </label>
+  				<input type="text" name="MessageContent" id="MessageContent" />
+  				<input type="submit" value="Miaou"/> 
   			</form>
   	</fieldset>
   </section>
@@ -35,9 +59,14 @@
   <section class="Suivre">
   		<fieldset>
   			<legend> Suivre le miaou: </legend>
-<? 
-include ("ChatThread.php");
-?>
+
+
+<?php while($LastTenMessages = $ChatThread->fetch()) : ?>
+	<p>
+	<strong><?= htmlspecialchars($LastTenMessages['Pseudo_maj']); ?></strong> :
+	<?= htmlspecialchars($LastTenMessages['Message'])?>
+	</p>
+<?php endwhile; ?>
 		</fieldset>
 	</section>
 
